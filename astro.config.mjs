80fa8cd8ef92d48846c30185012334f830e1d76e
Michael Gill <husingillchild@gmail.com>
2026-08-13 15:54:50 +1000

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,49 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+// Auto sidebar completion
+import starlightAutoSidebar from 'starlight-auto-sidebar';
+// katex support
+import { unified } from '@astrojs/markdown-remark';
+import remarkMath from 'remark-math';
+import rehypeKaTeX from 'rehype-katex';
+// Link validation support
+import starlightLinksValidator from 'starlight-links-validator';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  markdown: {
+    shikiConfig: {
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "kanagawa-wave",
+      langs: [],
+      wrap: true,
+    },
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKaTeX],
+    }),
+  },
+  integrations: [
+    starlight({
+      title: 'Monash University FIT Unit Template',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/orthosquare' }],
+      plugins: [
+        starlightAutoSidebar(), 
+        starlightLinksValidator({
+          errorOnRelativeLinks: true,
+        }),
+      ],
+      sidebar: [
+        {
+          label: 'Welcome',
+          items: [{ autogenerate: { directory: 'welcome' } }],
+        },
+      ],
+    }),
+  ],
+  server: {
+    host: true,
+    port: 3001,
+  },
 });
